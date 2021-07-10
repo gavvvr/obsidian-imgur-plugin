5bb4fcc1411d03e2bfe861ecd817fd764285a4ed
Kirill Gavrilov <gavvvr@ya.ru>
2021-07-10 18:28:04 +0300

    Fix build file name

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,7 @@ if you want to view the source visit the plugins github repository
 export default {
   input: "src/ImgurPlugin.ts",
   output: {
-    dir: ".",
+    file: "main.js",
     sourcemap: "inline",
     sourcemapExcludeSources: isProd,
     format: "cjs",
