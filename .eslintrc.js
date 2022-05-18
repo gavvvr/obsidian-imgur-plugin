0fc8f8e91b49d0b208a90c7be27967949f437a48
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2022-05-18 04:36:42 +0300

    chore(eslint): fix import warning in vitest.config.ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
+const airbnb = require("eslint-config-airbnb-typescript/lib/shared");
+
+const airbnbNoExtraDepsRule = airbnb.rules["import/no-extraneous-dependencies"];
+airbnbNoExtraDepsRule[1].devDependencies.push("vitest.config.ts");
+
 module.exports = {
   root: true,
   extends: ["airbnb-base", "prettier"],
@@ -19,6 +24,7 @@ module.exports = {
       ],
       rules: {
         "@typescript-eslint/no-explicit-any": "off",
+        "import/no-extraneous-dependencies": airbnbNoExtraDepsRule,
       },
     },
   ],
