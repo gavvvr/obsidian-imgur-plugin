4c884287374230d3f76ae0026e2d3bd089ab16e5
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2022-05-18 04:29:38 +0300

    chore(eslint): support mix of js and ts files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,25 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.eslint.json",
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "airbnb-base",
-    "airbnb-typescript/base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier",
+  extends: ["airbnb-base", "prettier"],
+
+  overrides: [
+    {
+      files: ["**/*.ts"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+      },
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "airbnb-base",
+        "airbnb-typescript/base",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier",
+      ],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+      },
+    },
   ],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-  },
 };
