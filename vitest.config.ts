0bd981334d87103caaa30ba7224e12436ff12081
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2023-04-27 22:01:39 +0300

    chore: explicitly specify coverage tool to fix ts error

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
+      provider: 'c8',
       reporter: ['lcov', 'html'],
     },
     reporters: ['default', 'junit'],
