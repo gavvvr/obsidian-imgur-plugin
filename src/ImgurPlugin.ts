f6a29717ea01a376ac95e9041f0fa9941a25af5e
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2023-10-12 08:10:28 +0300

    refactor: switch from deprecated 'substr' to 'substring'

--- src/ImgurPlugin.ts
+++ src/ImgurPlugin.ts
@@ -219,7 +219,7 @@ export default class ImgurPlugin extends Plugin {
   }
 
   private async uploadFileAndEmbedImgurImage(file: File) {
-    const pasteId = (Math.random() + 1).toString(36).substr(2, 5)
+    const pasteId = (Math.random() + 1).toString(36).substring(2, 7)
     this.insertTemporaryText(pasteId)
 
     let imgUrl: string
