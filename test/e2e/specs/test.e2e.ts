b3c07ca0c87f29d78c4d83bf95d50e27ce9f45af
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2024-04-08 22:04:54 +0300

    refactor(test,e2e): transform Obsidian scripts for e2e test into TS code

--- test/e2e/specs/test.e2e.ts
+++ test/e2e/specs/test.e2e.ts
@@ -1,5 +1,7 @@
 import * as fs from 'fs'
 import { Key } from 'webdriverio'
+import ImgurPlugin from '../../../src/ImgurPlugin'
+import { App } from 'obsidian'
 
 const TEST_VAULT_DIR = 'test/e2e/e2e_test_vault'
 const IMGUR_PLUGIN_ID = 'obsidian-imgur-plugin'
@@ -14,10 +16,12 @@ describe('Electron Testing', () => {
 
   context('blank note', () => {
     it('uploads clipboard image on PASTE shortcut', async () => {
-      await browser.execute(`
+      await browser.execute((imgurPluginId: typeof IMGUR_PLUGIN_ID) => {
+        // @ts-expect-error 'app' exists in Obsidian
+        declare const app: App
         const uploadStub = () => Promise.resolve('https://i.imgur.com/w88wB4I.png')
-        app.plugins.plugins['${IMGUR_PLUGIN_ID}'].imgUploader.upload = uploadStub
-      `)
+        app.plugins.plugins[imgurPluginId].imgUploader.upload = uploadStub
+      }, IMGUR_PLUGIN_ID)
 
       await createNewNoteAndFocusOnIt()
 
@@ -36,11 +40,12 @@ const removeTestVaultFromPreviousTestRun = () => {
 }
 
 const createAndOpenFreshTestVaultWithImgurPlugin = async () => {
-  const shouldCreateNewVault = true
-  await browser.execute(`
-    const {ipcRenderer} = require('electron')
-    ipcRenderer.sendSync('vault-open', '${TEST_VAULT_DIR}', ${shouldCreateNewVault})
-  `)
+  await browser.execute((testVaultDir: typeof TEST_VAULT_DIR) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { ipcRenderer } = require('electron')
+    const shouldCreateNewVault = true
+    ipcRenderer.sendSync('vault-open', testVaultDir, shouldCreateNewVault)
+  }, TEST_VAULT_DIR)
 
   const targetPluginsDir = `${TEST_VAULT_DIR}/.obsidian/plugins/${IMGUR_PLUGIN_ID}/`
   fs.mkdirSync(targetPluginsDir, { recursive: true })
@@ -58,10 +63,12 @@ const focusOnVaultOpenedWindow = async () => {
 }
 
 const activateImgurPlugin = async () => {
-  await browser.execute(`
+  await browser.execute((imgurPluginId: typeof IMGUR_PLUGIN_ID) => {
+    // @ts-expect-error 'app' exists in Obsidian
+    declare const app: App
     app.plugins.setEnable(true)
-    app.plugins.enablePlugin('${IMGUR_PLUGIN_ID}')
-  `)
+    app.plugins.enablePlugin(imgurPluginId)
+  }, IMGUR_PLUGIN_ID)
   await $('.modal-close-button').then((button) => button.click())
 }
 
@@ -74,15 +81,17 @@ const createNewNoteAndFocusOnIt = async () => {
 }
 
 const loadSampleImageToClipboard = async () => {
-  await browser.execute(`
-    const {nativeImage, clipboard} = require('electron')
-    const imageBase64 = 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD///+' +
-    'l2Z/dAAAAM0lEQVR4nGP4/5/h/1+' +
-    'G/58ZDrAz3D/McH8yw83NDDeNGe4Ug9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC'
+  await browser.execute(() => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { nativeImage, clipboard } = require('electron')
+    const imageBase64 =
+      'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD///+' +
+      'l2Z/dAAAAM0lEQVR4nGP4/5/h/1+' +
+      'G/58ZDrAz3D/McH8yw83NDDeNGe4Ug9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC'
     const dataUrl = 'data:image/png;base64,' + imageBase64
     const sampleImage = nativeImage.createFromDataURL(dataUrl)
     clipboard.writeImage(sampleImage)
-  `)
+  })
 }
 
 const pasteFromClipboard = async () => {
@@ -94,5 +103,22 @@ const confirmImageUpload = async () => {
 }
 
 const getTextFromOpenedNote = async () => {
-  return await browser.execute('return app.workspace.activeEditor.editor.getValue()')
+  return await browser.execute(() => {
+    // @ts-expect-error 'app' exists in Obsidian
+    declare const app: App
+    return app.workspace.activeEditor!.editor!.getValue()
+  })
+}
+
+declare module 'obsidian' {
+  interface App {
+    plugins: {
+      plugins: {
+        [index: string]: Plugin
+        [IMGUR_PLUGIN_ID]: ImgurPlugin
+      }
+      setEnable(toggle: boolean): void
+      enablePlugin(pluginId: string): void
+    }
+  }
 }
