fd8e9a2ec4858606d9e240f4d4401514711e2ffc
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2024-10-15 02:54:57 +0300

    chore(eslint): add imports sorting rules from eslint-plugin-perfectionist plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,44 @@
 // @ts-check
 
 import eslint from '@eslint/js'
+import perfectionist from 'eslint-plugin-perfectionist'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
+const sortImportsSettings = {
+  type: 'natural',
+  order: 'asc',
+  ignoreCase: false,
+  newlinesBetween: 'always',
+  groups: [
+    'type',
+    'builtin',
+    'external',
+    'internal-type',
+    'internal',
+    ['parent-type', 'sibling-type', 'index-type'],
+    ['parent', 'sibling', 'index'],
+    'object',
+    'unknown',
+  ],
+  environment: 'node',
+}
+
 export default tseslint.config(
   eslint.configs.recommended,
+  {
+    ...perfectionist.configs['recommended-natural'],
+    rules: {
+      'perfectionist/sort-imports': ['error', sortImportsSettings],
+      'perfectionist/sort-named-imports': [
+        'error',
+        {
+          type: 'alphabetical',
+          order: 'asc',
+        },
+      ],
+    },
+  },
   {
     files: ['**/*.ts'],
     extends: [
