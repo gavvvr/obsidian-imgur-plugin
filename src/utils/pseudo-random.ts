0aa796a3e9ccfe92dd748083a7c91cdcf6e36e2e
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2024-11-04 01:30:33 +0300

    refactor: new pseudo-random id implementation with guaranteed length

--- src/utils/pseudo-random.ts
+++ src/utils/pseudo-random.ts
@@ -1,5 +1,7 @@
-const FULL_ALPHANUMERIC_RADIX = 36
-const GENERATED_ID_MAX_LENGTH = 5
-
-export const generatePseudoRandomId = () =>
-  (Math.random() + 1).toString(FULL_ALPHANUMERIC_RADIX).substring(2, GENERATED_ID_MAX_LENGTH + 2)
+export const generatePseudoRandomId = (generatedIdLength = 5) => {
+  const fullAlphanumericRadix = 36
+  return Array(generatedIdLength)
+    .fill(undefined)
+    .map(() => ((Math.random() * fullAlphanumericRadix) | 0).toString(fullAlphanumericRadix))
+    .join('')
+}
