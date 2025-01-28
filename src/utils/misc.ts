c328240a20f35c9bc39a9b8a647748d91635f3cf
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2025-01-29 01:11:49 +0300

    refactor: extract code from ImgurPlugin class

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,5 @@
+import { EditorPosition, ReferenceCache } from 'obsidian'
+
 function fixImageTypeIfNeeded(image: File) {
   if (passesInstanceofCheck(image)) {
     return image
@@ -9,4 +11,25 @@ function fixImageTypeIfNeeded(image: File) {
   }
 }
 
-export { fixImageTypeIfNeeded }
+function removeReferenceIfPresent(
+  referencesByNote: Record<string, ReferenceCache[]>,
+  referenceToRemove: { path: string; startPosition: EditorPosition },
+) {
+  if (!Object.keys(referencesByNote).includes(referenceToRemove.path)) return
+
+  const refsFromOriginalNote = referencesByNote[referenceToRemove.path]
+  const originalRefStart = referenceToRemove.startPosition
+  const refForExclusion = refsFromOriginalNote.find(
+    (r) =>
+      r.position.start.line === originalRefStart.line &&
+      r.position.start.col === originalRefStart.ch,
+  )
+  if (refForExclusion) {
+    refsFromOriginalNote.remove(refForExclusion)
+    if (refsFromOriginalNote.length === 0) {
+      delete referencesByNote[referenceToRemove.path]
+    }
+  }
+}
+
+export { fixImageTypeIfNeeded, removeReferenceIfPresent }
